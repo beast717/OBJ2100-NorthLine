ce3cae30d334d70dc82f4de259fe108f1c120ac7
Guleed Dirshe <116796919+gdirshe@users.noreply.github.com>
2026-05-19 15:00:47 +0200

    Legger til kommentar i server

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -22,13 +22,16 @@ public class ClientHandler implements Runnable {
         this.ticService = ticService;
         this.log = log;
     }
-
+  
+    // Hovedløkken for å håndtere klientforespørsler
     @Override
     public void run() {
-
+        
+        // Logg tilkobling
         String clientAddress = clientSocket.getRemoteSocketAddress().toString();
         log.log("CLIENT_CONNECTED", null, clientAddress, null);
-
+     
+        // Bruk try-with-resources for å sikre at strømmer og socket lukkes riktig
         try (ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
              ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream())) {
 
@@ -37,14 +40,16 @@ public class ClientHandler implements Runnable {
                 Object obj = in.readObject();
 
                 if (obj instanceof RequestMessage) {
-
+                    
+                    // Håndter forespørselen og send svar
                     RequestMessage req = (RequestMessage) obj;
                     ResponseMessage res = ticService.handleRequest(req);
                     out.writeObject(res);
                     out.flush();
 
                 } else {
-
+                    
+                    // Mottok en melding av ukjent type – logg og send feilmelding
                     ResponseMessage error = new ResponseMessage(
                         ResponseStatus.ERROR_UNKNOWN_OPERATION,
                         "Ukjent meldingstype",
@@ -56,11 +61,16 @@ public class ClientHandler implements Runnable {
                 }
 
             }
-
+        
+            // Når klienten kobler fra, vil readObject() kaste EOFException, og vi kan håndtere det i catch-blokken
         } catch (EOFException e) {
-            // Klient koblet fra normalt
+
+            // Klienten har koblet fra – logg og avslutt tråden
         } catch (IOException | ClassNotFoundException e) {
+
             System.err.println("Feil i ClientHandler: " + e.getMessage());
+       
+            // Uansett hvordan løkken avsluttes, logg at klienten har koblet fra og lukk socketen
         } finally {
 
             log.log("CLIENT_DISCONNECTED", null, clientAddress, null);
